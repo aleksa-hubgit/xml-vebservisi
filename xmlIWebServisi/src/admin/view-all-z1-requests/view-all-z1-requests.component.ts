ce8ef68f67a6c71469bfe2aedb8d3a08313a771b
aleksa-hubgit <aleksa.stev92@gmail.com>
2023-06-11 19:46:51 +0200

    Dodata biblioteka za parsiranje xml-ova

--- xmlIWebServisi/src/admin/view-all-z1-requests/view-all-z1-requests.component.ts
+++ xmlIWebServisi/src/admin/view-all-z1-requests/view-all-z1-requests.component.ts
@@ -3,69 +3,65 @@ import { Location } from '@angular/common';
 import { ZigService } from 'src/app/z1/zig.service';
 import { Prijava } from 'src/app/model/Prijava';
 
-
 export interface TableData {
   ime: string;
   prezime: string;
   tip_ziga: string;
   opis_znaka: string;
-  sifraZahteva:string;
+  sifraZahteva: string;
 }
 
 @Component({
   selector: 'app-view-all-z1-requests',
   templateUrl: './view-all-z1-requests.component.html',
-  styleUrls: ['./view-all-z1-requests.component.css']
+  styleUrls: ['./view-all-z1-requests.component.css'],
 })
-
-
 export class ViewAllZ1RequestsComponent {
+  constructor(private service: ZigService, private location: Location) {}
 
-  constructor(private service : ZigService,private location: Location) {}
-  
-  tableData: Prijava[] = [
+  tableData: Prijava[] = [];
+
+  displayedColumns: string[] = [
+    'ime',
+    'prezime',
+    'tip_ziga',
+    'opis_znaka',
+    'detalji',
+    'prihvati',
+    'odbi',
+    'preuzmi',
   ];
 
-  displayedColumns: string[] = ['ime', 'prezime', 'tip_ziga', 'opis_znaka', 'detalji','prihvati' ,'odbi', 'preuzmi'];
-  
-  public viewDetails(a:string){
-    alert(a)
+  public viewDetails(a: string) {
+    alert(a);
   }
-  
+
   ngOnInit(): void {
+    this.service.parseString('');
     this.service.getZahtevi().subscribe({
-    next: async (xml) => {
-      this.tableData = this.service.parseString(xml);
-  }});
+      next: async (xml) => {
+        this.tableData = this.service.parseString(xml);
+      },
+    });
   }
 
-
-  
-  public Accept(id : string){
-
-    this.service.AcceptRequest(id,"").subscribe();
+  public Accept(id: string) {
+    this.service.AcceptRequest(id, '').subscribe();
     this.Refresh();
   }
 
-  public Decline(id : string){
-    this.service.DeclineRequest(id,"").subscribe();
+  public Decline(id: string) {
+    this.service.DeclineRequest(id, '').subscribe();
     this.Refresh();
   }
 
-  
-
-  public Print(id : Number){
+  public Print(id: Number) {
     this.service.Print(id).subscribe();
-
-    
   }
   public Refresh(): void {
     this.location.go(this.location.path());
   }
-
-  
 }
-
 
 class Adresa {
   ulica?: string;
--- xmlIWebServisi/src/app/z1/zig.service.ts
+++ xmlIWebServisi/src/app/z1/zig.service.ts
@@ -4,24 +4,27 @@ import { Prijava } from '../model/Prijava';
 import * as JsonToXML from 'js2xmlparser';
 import { Decision } from '../model/Decision';
 
+import * as Parser from 'xml-js';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ZigService {
-
   constructor(private _http: HttpClient) {}
-  
-  
-  
-  
-  
+
   parseString(xml: string): Prijava[] {
-    return  [ new Prijava()]
+    xml = `<note>
+    <to>Tove</to>
+    <from>Jani</from>
+    <heading>Reminder</heading>
+    <body>Don't forget me this weekend!</body>
+    </note>`;
+    const result = Parser.xml2js(xml, { compact: true });
+    console.log(result);
+    return [new Prijava()];
   }
 
-
   url = 'http://localhost:9000/trademark/';
-
 
   sendRequest(prijava: Prijava) {
     const xml = JsonToXML.parse('prijavaZaZig', prijava);
@@ -35,46 +38,46 @@ export class ZigService {
     });
   }
 
-  Print(id: Number) { const xml = JsonToXML.parse('broj', id);
-  const url = this.url + 'print';
-  return this._http.post<any>(url, xml, {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/xml',
-      'Access-Control-Allow-Origin': '*',
+  Print(id: Number) {
+    const xml = JsonToXML.parse('broj', id);
+    const url = this.url + 'print';
+    return this._http.post<any>(url, xml, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/xml',
+        'Access-Control-Allow-Origin': '*',
+        responseType: 'text',
+      }),
+    });
+  }
+  DeclineRequest(id: string, obrazlozenje: string) {
+    let d: Decision = new Decision(id, obrazlozenje, false);
+    const xml = JsonToXML.parse('decision', d);
+    const url = this.url + 'handleRequest';
+    return this._http.post<any>(url, xml, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/xml',
+        'Access-Control-Allow-Origin': '*',
+        responseType: 'text',
+      }),
+    });
+  }
+  AcceptRequest(id: string, obrazlozenje: string) {
+    let d: Decision = new Decision(id, obrazlozenje, true);
+    const xml = JsonToXML.parse('decision', d);
+    const url = this.url + 'handleRequest';
+    return this._http.post<any>(url, xml, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/xml',
+        'Access-Control-Allow-Origin': '*',
+        responseType: 'text',
+      }),
+    });
+  }
+
+  getZahtevi() {
+    return this._http.get(this.url + 'getAll', {
+      headers: new HttpHeaders().set('Content-Type', 'application/xml'),
       responseType: 'text',
-    }),
-  });
-  }
-  DeclineRequest(id: string,obrazlozenje:string) {
-    let d:Decision = new Decision(id,obrazlozenje,false)
-    const xml = JsonToXML.parse('decision', d);
-    const url = this.url + 'handleRequest';
-    return this._http.post<any>(url, xml, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/xml',
-        'Access-Control-Allow-Origin': '*',
-        responseType: 'text',
-      }),
     });
   }
-  AcceptRequest(id: string,obrazlozenje:string) {
-    let d:Decision = new Decision(id,obrazlozenje,true)
-    const xml = JsonToXML.parse('decision', d);
-    const url = this.url + 'handleRequest';
-    return this._http.post<any>(url, xml, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/xml',
-        'Access-Control-Allow-Origin': '*',
-        responseType: 'text',
-      }),
-    });
-}
-
-getZahtevi() {
-  return this._http.get(this.url + 'getAll', {
-    headers: new HttpHeaders().set('Content-Type', 'application/xml'),
-    responseType: 'text',
-  });
-}
-
 }
