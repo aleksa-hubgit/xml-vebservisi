bde506d12002d8fb097b95e2a06770272b3ee336
aleksa-hubgit <aleksa.stev92@gmail.com>
2023-06-11 23:09:54 +0200

    Gotova forma za patent

---
+++ xmlIWebServisi/src/app/p1/p1.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { PatentRequestDTO } from '../model/p1/PatentRequestDTO';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as JsonToXML from 'js2xmlparser';
+@Injectable({
+  providedIn: 'root',
+})
+export class P1Service {
+  constructor(private _http: HttpClient) {}
+  url = 'http://localhost:9002/patent/';
+
+  sendRequest(prijava: PatentRequestDTO) {
+    const xml = JsonToXML.parse('autorskaPrijava', prijava);
+    const url = this.url + 'createRequest';
+    return this._http.post<any>(url, xml, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/xml',
+        'Access-Control-Allow-Origin': '*',
+        responseType: 'text',
+      }),
+    });
+  }
+}
